2681f932257314a138daecb254332fddf35cb3ea
Kevin Eduardo Cardenas mamani <kevin.kc541@gmail.com>
2022-06-23 16:15:45 -0400

    Agregar archivos de proyecto.

---
+++ Intro/Intro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numero = 15;
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	char letra = 'a';
+	char letra2 = '1';
+
+	cout << "Un ejemplo de int es: " << numero << endl;
+	cout << "Un ejemplo de float es: " << flotante << "\n";
+	cout << "Un ejemplo de double es:" << mayor << endl;
+	cout << "Un ejemplo de char es:" << letra << "\n";
+	cout << "Otro ejemplo de char es:" << letra2 << "\n";
+
+	return 0;
+}
